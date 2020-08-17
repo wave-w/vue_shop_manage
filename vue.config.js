d9037dce5c6467c21e640d288d244ab850dc82b8
吴波 <1950566932@qq.com>
2020-08-17 19:50:00 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    configureWebpack: {
+        resolve:{
+          extensions:[],
+          alias:{
+            'assets':'@/assets',
+            'common':'@/common',
+            'components':'@/components',
+            'network':'@/network',
+            'view':'@/view',
+          }
+        }
+      },
+    devServer:{
+           host: '0.0.0.0',
+           public:'192.168.124.4:8080',
+           port: 8080,
+           https: false,
+           https: false,
+           disableHostCheck:true,
+           open: true 
+        }
+}
